262431865fb461cde1bd61977d49b6ec1b728187
Sang <106306044+zzzz955@users.noreply.github.com>
2025-03-01 22:08:41 +0900

    [Gold IV] Title: 사이클 게임, Time: 84 ms, Memory: 3972 KB -BaekjoonHub

--- C++17/백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ C++17/백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -1,20 +1,22 @@
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
+const int N = 500000;
 int n, m;
-int nodes[500001];
+int nodes[N];
 
 int Find(int a) {
 	if (nodes[a] == a) return a;
 	return nodes[a] = Find(nodes[a]);
 }
 
-void Union(int a, int b) {
-	int rootA = Find(a);
-	int rootB = Find(b);
-	if (rootA == rootB) return;
-	nodes[rootB] = rootA;
+bool Union(int a, int b) {
+	int A = Find(a);
+	int B = Find(b);
+	if (A == B) return true;
+	nodes[B] = A;
+	return false;
 }
 
 int main() {
@@ -22,15 +24,15 @@ int main() {
 	cin.tie(0);
 
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) nodes[i] = i;
-	for (int i = 1; i <= m; i++) {
+	for (int i = 0; i < n; ++i) nodes[i] = i;
+
+	for (int i = 1; i <= m; ++i) {
 		int a, b; cin >> a >> b;
-		if (Find(a) == Find(b)) {
+		if (a > b) swap(a, b);
+		if (Union(a, b)) {
 			cout << i;
 			return 0;
 		}
-		Union(a, b);
 	}
 	cout << 0;
-	return 0;
 }
