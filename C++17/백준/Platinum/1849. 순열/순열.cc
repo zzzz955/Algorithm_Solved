f33e9d37d65381f0cc5a3b7a188293d784e9165d
Sang <106306044+zzzz955@users.noreply.github.com>
2025-04-01 09:12:28 +0900

    [Platinum IV] Title: 순열, Time: 28 ms, Memory: 3972 KB -BaekjoonHub

---
+++ C++17/백준/Platinum/1849. 순열/순열.cc
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+const int N = 1e5 + 1;
+int n, s;
+int lst[N];
+int tree[N * 4];
+
+void build(int node, int s, int e) {
+	if (s == e) tree[node] = lst[s];
+	else {
+		int mid = (s + e) / 2;
+		build(node * 2, s, mid);
+		build(node * 2 + 1, mid + 1, e);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+}
+
+void update(int node, int s, int e, int idx) {
+	if (s == e) --tree[node];
+	else {
+		int mid = (s + e) / 2;
+		if (idx <= mid) update(node * 2, s, mid, idx);
+		else update(node * 2 + 1, mid + 1, e, idx);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+}
+
+int query(int node, int s, int e, int val) {
+	if (s == e) return s;
+	int mid = (s + e) / 2;
+	if (val < tree[node * 2]) return query(node * 2, s, mid, val);
+	return query(node * 2 + 1, mid + 1, e, val - tree[node * 2]);
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 1; i <= n; ++i) lst[i] = 1;
+	build(1, 1, n);
+
+	for (int i = 1; i <= n; ++i) {
+		cin >> s;
+		int idx = query(1, 1, n, s);
+		//cout << "result of query : " << idx << "\n";
+		lst[idx] = i;
+		update(1, 1, n, idx);
+	}
+
+	for (int i = 1; i <= n; ++i) cout << lst[i] << "\n";
+}
