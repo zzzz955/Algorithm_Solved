f926d88b530f062e3ceb8fdb76ebd3e92fc04067
Sang <106306044+zzzz955@users.noreply.github.com>
2025-05-08 09:07:53 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.cc
+++ C++17/백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.cc
@@ -1,10 +1,8 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
 int n;
-pair<int,int> h[50000];
 vector<int> stack;
 
 int main() {
@@ -12,15 +10,9 @@ int main() {
 	cin.tie(0);
 
 	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int x, y; cin >> x >> y;
-		h[i] = { x, y };
-	}
-	sort(h, h + n);
-
 	int ans = 0;
-	for (int i = 0; i < n; ++i) {
-		int y = h[i].second;
+	while (n--) {
+		int x, y; cin >> x >> y;
 		while (!stack.empty() && stack.back() >= y) {
 			if (stack.back() > y) ans++;
 			stack.pop_back();
