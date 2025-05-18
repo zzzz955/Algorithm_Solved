b460886d04f93766f36d3771ad2ce0bb4a5d98da
Sang <106306044+zzzz955@users.noreply.github.com>
2025-05-18 15:27:25 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 188 ms, Memory: 20016 KB, Score: 18 point -BaekjoonHub

--- Java/백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
+++ Java/백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, M, A, B, C;
+    static int N, M, A, B, C, ans;
     static class Edge {
         int nn, nv;
         Edge(int nn, int nv) {
@@ -13,40 +13,22 @@ class Main {
         }
     }
     static List<List<Edge>> edges = new ArrayList<>();
-    static class Pos {
-        int cn, cv, mv;
-        Pos(int cn, int cv, int mv) {
-            this.cn = cn;
-            this.cv = cv;
-            this.mv = mv;
-        }
-    }
+    static boolean[] v = new boolean[11];
     
-    static int dijkstra() {
-        PriorityQueue<Pos> pq = new PriorityQueue<>((a, b) -> a.cv - b.cv);
-        pq.offer(new Pos(A, 0, 0));
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, 2000000000);
-        dist[A] = 0;
-        int min_val = 2000000000;
-        
-        while (!pq.isEmpty()) {
-            Pos pos = pq.poll();
-            int cn = pos.cn, cv = pos.cv, mv = pos.mv;
-            if (dist[cn] < cv) continue;
-            
-            for (Edge edge : edges.get(cn)) {
-                int nn = edge.nn, nv = edge.nv;
-                if (cv + nv > C) continue;
-                if (dist[nn] > cv + nv) {
-                    int new_mv = Math.max(mv, nv);
-                    if (nn == B && min_val > new_mv) min_val = new_mv;
-                    dist[nn] = cv + nv;
-                    pq.offer(new Pos(nn, cv + nv, new_mv));
-                }
-            }
+    static void dfs(int cn, int spent, int mv) {
+        if (cn == B) {
+            if (ans == -1 || ans > mv) ans = mv;
+            return;
         }
-        return dist[B] == 2000000000 ? -1 : min_val;
+        
+        for (Edge edge : edges.get(cn)) {
+            int nn = edge.nn, nv = edge.nv;
+            if (v[nn]) continue;
+            if (spent + nv > C) continue;
+            v[nn] = true;
+            dfs(nn, spent + nv, Math.max(mv, nv));
+            v[nn] = false;
+        }
     }
     
     public static void main(String[] args) throws IOException {
@@ -67,7 +49,11 @@ class Main {
             edges.get(b).add(new Edge(a, c));
         }
         
-        bw.write(dijkstra() + "\n");
+        ans = -1;
+        v[A] = true;
+        dfs(A, 0, 0);
+        
+        bw.write(ans + "\n");
         br.close();
         bw.close();
     }
