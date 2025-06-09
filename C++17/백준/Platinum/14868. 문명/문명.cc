73dda516241454d420be9f506af2a035cb563cac
Sang <106306044+zzzz955@users.noreply.github.com>
2025-06-09 21:20:06 +0900

    [Platinum IV] Title: 문명, Time: 1820 ms, Memory: 85252 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Platinum/14868. 문명/문명.cc
+++ C++17/백준/Platinum/14868. 문명/문명.cc
@@ -7,18 +7,17 @@ const int N = 4002;
 const int K = 100001;
 int n, k;
 int lst[N][N];
-bool v[N][N];
 struct Pos {
 	int x, y, g;
 };
 queue<Pos> flood;
-int groups[K];
+int parent[K];
 int dx[] = { 1, -1, 0, 0 };
 int dy[] = { 0, 0, 1, -1 };
 
 int Find(int a) {
-	if (groups[a] == a) return a;
-	return groups[a] = Find(groups[a]);
+	if (parent[a] == a) return a;
+	return parent[a] = Find(parent[a]);
 }
 
 void Union(int a, int b) {
@@ -26,14 +25,14 @@ void Union(int a, int b) {
 	int B = Find(b);
 	if (A == B) return;
 	if (A > B) swap(A, B);
-	groups[B] = A;
+	parent[B] = A;
 	k--;
 	//cout << a << " " << b << " " << k << "\n";
 }
 
 void floodfill() {
 	queue<Pos> q;
-	swap(flood, q);
+	swap(q, flood);
 
 	while (!q.empty()) {
 		Pos pos = q.front(); q.pop();
@@ -42,12 +41,13 @@ void floodfill() {
 		for (int i = 0; i < 4; ++i) {
 			int nx = cx + dx[i], ny = cy + dy[i];
 			if (2 <= nx && nx <= n * 2 && 2 <= ny && ny <= n * 2) {
-				if (!v[nx][ny]) {
-					v[nx][ny] = true;
+				if (!lst[nx][ny]) {
 					lst[nx][ny] = cg;
 					flood.push({ nx, ny, cg });
 				}
-				else Union(cg, lst[nx][ny]);
+				else if (Find(cg) != Find(lst[nx][ny])) {
+					Union(cg, lst[nx][ny]);
+				}
 			}
 		}
 	}
@@ -83,9 +83,8 @@ int main() {
 		int x, y; cin >> x >> y;
 		x *= 2;
 		y *= 2;
-		v[x][y] = true;
 		lst[x][y] = i;
-		groups[i] = i;
+		parent[i] = i;
 		flood.push({ x, y, i });
 	}
 
