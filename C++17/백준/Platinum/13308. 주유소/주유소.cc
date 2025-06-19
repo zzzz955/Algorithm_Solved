8eaa8e4894447a0af7d579a7d745a47e04974e19
Sang <106306044+zzzz955@users.noreply.github.com>
2025-06-19 13:55:54 +0900

    [Platinum V] Title: 주유소, Time: 40 ms, Memory: 51220 KB, Score: 73 point -BaekjoonHub

---
+++ C++17/백준/Platinum/13308. 주유소/주유소.cc
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#define ll long long
+using namespace std;
+
+const int N = 2501;
+int n, m;
+int p[N];
+struct Edge {
+	int nn, nv;
+};
+vector<Edge> edges[N];
+struct Pos {
+	int cn, cf;
+	ll cc;
+	bool operator<(const Pos& other) const {
+		return cc > other.cc;
+	}
+};
+
+ll dijkstra() {
+	priority_queue<Pos> pq;
+	pq.push({ 1, p[1], 0 });
+	vector<vector<ll>> dist(n + 1, vector<ll>(N, 1e11));
+	dist[1][p[1]] = 0;
+
+	while (!pq.empty()) {
+		Pos pos = pq.top(); pq.pop();
+		int cn = pos.cn, cf = pos.cf;
+		ll cc = pos.cc;
+		//cout << cn << " " << cf << " " << cc << "\n";
+		if (cn == n) return cc;
+
+		for (const Edge& edge : edges[cn]) {
+			int nn = edge.nn;
+			int nf = min(cf, p[nn]);
+			ll nc = cc + cf * edge.nv;
+
+			if (dist[nn][nf] > nc) {
+				dist[nn][nf] = nc;
+				pq.push({ nn, nf, nc });
+			}
+		}
+	}
+
+	ll ans = 1e11;
+	for (int i = 1; i <= N; ++i) ans = min(ans, dist[n][i]);
+	return ans;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i) cin >> p[i];
+	while (m--) {
+		int a, b, c; cin >> a >> b >> c;
+		edges[a].push_back({ b, c });
+		edges[b].push_back({ a, c });
+	}
+
+	cout << dijkstra();
+}
