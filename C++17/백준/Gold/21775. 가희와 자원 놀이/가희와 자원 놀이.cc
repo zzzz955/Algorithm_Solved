b37437fd3a8405b2343447501d4d2387bf7bdf73
Sang <106306044+zzzz955@users.noreply.github.com>
2025-07-08 20:09:50 +0900

    [Gold V] Title: 가희와 자원 놀이, Time: 784 ms, Memory: 368120 KB -BaekjoonHub

--- C++17/백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.cc
+++ C++17/백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.cc
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<queue>
-#include<set>
+#include<unordered_set>
 using namespace std;
 
 const int N = 5e5 + 1;
+const int BIT_SIZE = 2000000000 / 8 + 1;
+char common_bitmap[BIT_SIZE];
+
 int n, t;
-set<int> common;
-set<int> user[N];
+unordered_set<int> user[N];
 struct Card {
 	int id;
 	string op;
@@ -14,19 +16,33 @@ struct Card {
 };
 Card hand[N];
 
+inline bool is_set(int num) {
+	return common_bitmap[num >> 3] & (1 << (num & 7));
+}
+
+inline void set_bit(int num) {
+	common_bitmap[num >> 3] |= (1 << (num & 7));
+}
+
+inline void clear_bit(int num) {
+	common_bitmap[num >> 3] &= ~(1 << (num & 7));
+}
+
 bool do_turn(int pid, Card card) {
 	int id = card.id;
 	string op = card.op;
 	int num = card.num;
 
 	if (op == "next") return true;
+
 	if (op == "release") {
 		user[pid].erase(num);
-		common.erase(num);
+		clear_bit(num);
 		return true;
 	}
-	if (!common.count(num)) {
-		common.insert(num);
+
+	if (!is_set(num)) {
+		set_bit(num);
 		user[pid].insert(num);
 		return true;
 	}
@@ -58,7 +74,7 @@ int main() {
 
 	while (!turn.empty()) {
 		int cu = turn.front(); turn.pop();
-		
+
 		if (!hand[cu].id) {
 			Card card = cards.front(); cards.pop();
 			cout << card.id << "\n";
