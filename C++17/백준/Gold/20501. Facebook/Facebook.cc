e4aaba7a98bf143010abe5b2b204333a4514b921
Sang <106306044+zzzz955@users.noreply.github.com>
2025-07-18 11:24:05 +0900

    [Gold II] Title: Facebook, Time: 220 ms, Memory: 2520 KB -BaekjoonHub

--- C++17/백준/Gold/20501. Facebook/Facebook.cc
+++ C++17/백준/Gold/20501. Facebook/Facebook.cc
@@ -1,10 +1,11 @@
 #include<iostream>
+#define ull unsigned long long
 using namespace std;
 
 const int N = 2000;
-const int B = 250;
+const int B = 32;
 int n, q;
-char bits[N][B];
+ull bits[N][B];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -15,21 +16,18 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
 			char ch; cin >> ch;
-			if (ch == '1') bits[i][j / 8] |= 1 << (j % 8);
+			if (ch == '1') bits[i][j / 64] |= 1ull << (j % 64);
 		}
 	}
 	
 	cin >> q;
-	int m = n % 8 ? n / 8 + 1 : n / 8;
+	int m = n % 64 ? n / 64+ 1 : n / 64;
 	while (q--) {
 		int a, b; cin >> a >> b;
 		int ans = 0;
 		--a; --b;
 		for (int i = 0; i < m; ++i) {
-			char res = bits[a][i] & bits[b][i];
-			for (int j = 0; j < 8; ++j) {
-				if (res & (1 << j)) ans++;
-			}
+			ans += __builtin_popcountll(bits[a][i] & bits[b][i]);
 		}
 		cout << ans << "\n";
 	}
