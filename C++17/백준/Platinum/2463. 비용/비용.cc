e0b7fc4f91c0e48a4f8869aed7cbf726c5bef9be
Sang <106306044+zzzz955@users.noreply.github.com>
2025-07-21 11:03:09 +0900

    [Platinum IV] Title: 비용, Time: 36 ms, Memory: 5236 KB -BaekjoonHub

--- C++17/백준/Platinum/2463. 비용/비용.cc
+++ C++17/백준/Platinum/2463. 비용/비용.cc
@@ -9,15 +9,14 @@ const ll MOD = 1e9;
 int n, m;
 ll ans, total;
 struct Edge {
-	int u, v;
-	ll c;
+	int u, v, c;
 	bool operator<(const Edge& other) const {
 		return c > other.c;
 	}
 };
 vector<Edge> edges;
 int nodes[N];
-ll sizes[N];
+int sizes[N];
 
 int Find(int a) {
 	if (nodes[a] == a) return a;
@@ -30,18 +29,15 @@ void Union(int a, int b) {
 	if (A == B) return;
 	if (sizes[A] < sizes[B]) swap(A, B);
 	nodes[B] = A;
-
-	ll c = (sizes[A] % MOD) * (sizes[B] % MOD) % MOD;
-	c = c * (total % MOD) % MOD;
-	ans = (ans + c) % MOD;
-
+	ans += total * sizes[A] * sizes[B];
+	ans %= MOD;
 	sizes[A] += sizes[B];
+	
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	cin >> n >> m;
 	for (int i = 1; i <= n; ++i) {
 		nodes[i] = i;
@@ -49,19 +45,16 @@ int main() {
 	}
 
 	while (m--) {
-		int u, v;
-		ll c;
-		cin >> u >> v >> c;
+		int u, v, c; cin >> u >> v >> c;
 		edges.push_back({ u, v, c });
 		total += c;
 	}
-
 	sort(edges.begin(), edges.end());
+
 	for (const Edge& edge : edges) {
 		int u = edge.u, v = edge.v, c = edge.c;
 		Union(u, v);
 		total -= c;
-		
 	}
 	cout << ans;
 }
