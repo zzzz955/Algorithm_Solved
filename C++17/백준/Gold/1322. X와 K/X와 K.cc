ae0bc9a764086fe8cc65a56a4fdd4f0d5b57544c
Sang <106306044+zzzz955@users.noreply.github.com>
2025-09-17 17:32:21 +0900

    [Gold IV] Title: X와 K, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/1322. X와 K/X와 K.cc
+++ C++17/백준/Gold/1322. X와 K/X와 K.cc
@@ -1,26 +1,17 @@
 #include<iostream>
 using namespace std;
-using ull = unsigned long long;
+using ll = long long;
+
+ll x, k, y, i, j;
 
 int main() {
-	ull x, k, y = 0; cin >> x >> k;
-	int i = 0, j = 0;
-
+	cin >> x >> k;
 	while ((k >> j) > 0 || (x >> i) > 0) {
 		if (!((x >> i) & 1)) {
-			if (((k >> j) & 1)) y |= ((ull)1 << i);
+			if (((k >> j) & 1)) y |= ((ll)1 << i);
 			j++;
 		}
 		i++;
 	}
 	cout << y;
 }
-
-//65535 1000000000
-//65536000000000
-
-//65535 2000000000
-//131072000000000
-
-//1073741823 2000000000
-//2147483648000000000
