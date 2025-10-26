297dbcf45d91e8221fdd4d1a5d8432c278b193e0
Sang <106306044+zzzz955@users.noreply.github.com>
2025-10-26 20:42:56 +0900

    [Gold IV] Title: 회전 초밥, Time: 220 ms, Memory: 13752 KB -BaekjoonHub

--- C++17/백준/Gold/15961. 회전 초밥/회전 초밥.cc
+++ C++17/백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<unordered_map>
 using namespace std;
 
 const int N = 3e6;
+const int D = 3e3 + 1;
 int n, d, k, c;
 int lst[N];
-unordered_map<int, int> dic;
+int dic[D];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -14,24 +14,22 @@ int main() {
 	cin >> n >> d >> k >> c;
 	for (int i = 0; i < n; ++i) cin >> lst[i];
 
-	for (int i = 0; i < k; ++i) dic[lst[i]]++;
-	int ans = dic.count(c) ? dic.size() : dic.size() + 1;
+	int uniq = 0;
+	for (int i = 0; i < k; ++i) if (++dic[lst[i]] == 1) ++uniq;
+	int ans = uniq + (dic[c] > 0 ? 0 : 1);
 
 	int L = 0, R = k;
 	for (int i = 0; i < n; ++i) {
 		int del = lst[L];
-		if (--dic[del] == 0) dic.erase(del);
+		if (--dic[del] == 0) --uniq;
 		if (++L == n) L = 0;
 
 		int add = lst[R];
-		dic[add]++;
+		if (++dic[add] == 1) ++uniq;
 		if (++R == n) R = 0;
 
-		int cnt = dic.count(c) ? dic.size() : dic.size() + 1;
-		//cout << "==========\n";
-		//for (const auto& j : dic) cout << j.first << ":" << j.second << "\n";
-		//cout << ans << " " << cnt << "\n";
-		ans = max(ans, cnt);
+		int cnt = uniq + (dic[c] > 0 ? 0 : 1);
+		if (ans < cnt) ans = cnt;
 	}
 	cout << ans;
 }
