969515eac33ef7a4cc4a40e46bae640a008134d6
Sang <106306044+zzzz955@users.noreply.github.com>
2025-10-26 21:41:05 +0900

    [Gold V] Title: 용액 합성하기, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++17/백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int N = 1e5;
+int lst[N];
+int n;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) cin >> lst[i];
+	sort(lst, lst + n);
+
+	int l = 0, r = n - 1, ans = 1e9;
+	while (l < r) {
+		int sum = lst[l] + lst[r];
+		if (abs(ans) > abs(sum)) ans = sum;
+		if (sum > 0) r--;
+		else if (sum < 0) l++;
+		else break;
+	}
+	cout << ans;
+}
