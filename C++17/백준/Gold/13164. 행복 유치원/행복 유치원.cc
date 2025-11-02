fc791f6016d0086220be52d1836e8e6be11c9a54
Sang <106306044+zzzz955@users.noreply.github.com>
2025-11-02 18:53:55 +0900

    [Gold V] Title: 행복 유치원, Time: 68 ms, Memory: 6392 KB -BaekjoonHub

--- C++17/백준/Gold/13164. 행복 유치원/행복 유치원.cc
+++ C++17/백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<algorithm>
 #include<queue>
 using namespace std;
 
@@ -13,7 +12,6 @@ int main() {
 
 	cin >> n >> k;
 	for (int i = 0; i < n; ++i) cin >> lst[i];
-	sort(lst, lst + n);
 
 	priority_queue<int> pq;
 	for (int i = 1; i < n; ++i) {
