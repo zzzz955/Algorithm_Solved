803b5746f698f1bad5fcd362cd10ba72a8627aad
Sang <106306044+zzzz955@users.noreply.github.com>
2025-11-17 16:20:12 +0900

    [Gold III] Title: 시장 선거 포스터, Time: 20 ms, Memory: 4088 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2370. 시장 선거 포스터/시장 선거 포스터.cc
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+using pii = pair<int, int>;
+
+const int N = 2e4 + 1;
+int n;
+vector<pii> cs;
+vector<int> xs;
+int tree[N * 4];
+int lazy[N * 4];
+
+void propagate(int node, int s, int e) {
+	if (lazy[node]) {
+		tree[node] = lazy[node];
+		if (s != e) {
+			lazy[node * 2] = lazy[node];
+			lazy[node * 2 + 1] = lazy[node];
+		}
+		lazy[node] = 0;
+	}
+}
+
+void update(int node, int s, int e, int L, int R, int v) {
+	if (R < s || e < L) return;
+	if (L <= s && e <= R) {
+		lazy[node] = v;
+		propagate(node, s, e);
+		return;
+	}
+	propagate(node, s, e);
+	int mid = (s + e) / 2;
+	update(node * 2, s, mid, L, R, v);
+	update(node * 2 + 1, mid + 1, e, L, R, v);
+}
+
+set<int> query(int node, int s, int e, int L, int R) {
+	propagate(node, s, e);
+	if (s == e) return { tree[node] };
+	int mid = (s + e) / 2;
+	set<int> left = query(node * 2, s, mid, L, R);
+	set<int> right = query(node * 2 + 1, mid + 1, e, L, R);
+	left.insert(right.begin(), right.end());
+	return left;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		int l, r; cin >> l >> r;
+		cs.push_back({ l, r });
+		xs.push_back(l);
+		xs.push_back(r);
+	}
+
+	xs.push_back(0);
+	sort(xs.begin(), xs.end());
+	xs.erase(unique(xs.begin(), xs.end()), xs.end());
+
+	int m = xs.size();
+	for (int i = 1; i <= n; ++i) {
+		pii c = cs[i - 1];
+		int l = c.first, r = c.second;
+		int li = lower_bound(xs.begin(), xs.end(), l) - xs.begin();
+		int ri = lower_bound(xs.begin(), xs.end(), r) - xs.begin();
+		update(1, 1, m, li, ri, i);
+	}
+	set<int> ans = query(1, 1, m, 1, m);
+	if (ans.count(0)) ans.erase(0);
+	cout << ans.size();
+}
