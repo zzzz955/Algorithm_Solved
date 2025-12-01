5bffb0b38d36e81d4e9fd6b09163b34d282ed7a1
Sang <106306044+zzzz955@users.noreply.github.com>
2025-12-01 19:20:23 +0900

    [Gold IV] Title: Count Circle Groups, Time: 1744 ms, Memory: 2168 KB -BaekjoonHub

---
+++ C++17/백준/Gold/10216. Count Circle Groups/Count Circle Groups.cc
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+using namespace std;
+
+int t, n;
+struct Pos {
+	int x, y, r;
+};
+
+double get_dist(const Pos& a, const Pos& b) {
+	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+void bfs(int sn, const vector<vector<int>>& edges, vector<bool>& v) {
+	queue<int> q;
+	q.push(sn);
+	v[sn] = true;
+
+	while (!q.empty()) {
+		int cn = q.front(); q.pop();
+
+		for (int nn : edges[cn]) {
+			if (v[nn]) continue;
+			v[nn] = true;
+			q.push(nn);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> t;
+	while (t--) {
+		cin >> n;
+
+		vector<Pos> poses(n + 1);
+		for (int i = 1; i <= n; ++i) {
+			int x, y, r; cin >> x >> y >> r;
+			poses[i] = { x, y, r };
+		}
+
+		vector<vector<int>> edges(n + 1);
+		for (int i = 1; i < n; ++i) {
+			for (int j = i + 1; j <= n; ++j) {
+				Pos a = poses[i], b = poses[j];
+				double dist = get_dist(a, b);
+				if (dist <= poses[i].r + poses[j].r) {
+					edges[i].push_back(j);
+					edges[j].push_back(i);
+				}
+			}
+		}
+
+		vector<bool> v(n + 1);
+		int g = 0;
+		for (int i = 1; i <= n; ++i) {
+			if (v[i]) continue;
+			++g;
+			bfs(i, edges, v);
+		}
+
+		cout << g << "\n";
+	}
+}
