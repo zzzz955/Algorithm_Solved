68f8e61862ecef55b7964d6a3562d71fd0555d0f
Sang <106306044+zzzz955@users.noreply.github.com>
2025-12-06 21:24:41 +0900

    [Gold IV] Title: 성싶당 밀키트, Time: 688 ms, Memory: 7564 KB -BaekjoonHub

---
+++ C++17/백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.cc
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+using ll = long long;
+
+const int N = 2e5;
+int n, g, k;
+priority_queue<ll> A, B;
+struct Info {
+	int S, L;
+	bool O;
+};
+Info ing[N];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> g >> k;
+	for (int i = 0; i < n; ++i) {
+		int S, L;
+		bool O;
+		cin >> S >> L >> O;
+		ing[i] = { S, L, O };
+	}
+
+	ll l = 0, r = 2e9, ans = -1;
+	while (l <= r) {
+		ll mid = (l + r) / 2;
+		for (int i = 0; i < n; ++i) {
+			const Info& info = ing[i];
+			if (info.O) A.push(1ll * info.S * max(1ll, mid - info.L));
+			else B.push(1ll * info.S * max(1ll, mid - info.L));
+		}
+
+		ll sum = 0;
+		int remove = 0;
+		while (!B.empty()) {
+			ll val = B.top(); B.pop();
+			sum += val;
+		}
+
+		while (!A.empty()) {
+			ll val = A.top(); A.pop();
+			if (remove < k) {
+				++remove;
+				continue;
+			}
+			sum += val;
+		}
+
+		//cout << sum << "\n";
+		if (sum <= g) {
+			ans = mid;
+			l = mid + 1;
+		}
+		else r = mid - 1;
+	}
+	cout << ans;
+}
