9eac7e22b70a9687e29e36b621d1c029738c7b9e
Sang <106306044+zzzz955@users.noreply.github.com>
2025-12-10 19:08:09 +0900

    [Gold IV] Title: 친구비, Time: 4 ms, Memory: 2704 KB -BaekjoonHub

---
+++ C++17/백준/Gold/16562. 친구비/친구비.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+const int N = 10001;
+int n, m, k;
+int cost[N];
+bool v[N];
+vector<int> edges[N];
+int f;
+
+int bfs(int sn) {
+	queue<int> q;
+	q.push(sn);
+	int mn = cost[sn];
+	v[sn] = true;
+	++f;
+
+	while (!q.empty()) {
+		int cn = q.front(); q.pop();
+
+		for (int nn : edges[cn]) {
+			if (v[nn]) continue;
+			v[nn] = true;
+			++f;
+			mn = min(mn, cost[nn]);
+			q.push(nn);
+		}
+	}
+	return mn;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; ++i) cin >> cost[i];
+	
+	while (m--) {
+		int a, b; cin >> a >> b;
+		edges[a].push_back(b);
+		edges[b].push_back(a);
+	}
+
+	long long sum = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (v[i]) continue;
+		sum += bfs(i);
+	}
+	
+	if (f != n || sum > k) cout << "Oh no";
+	else cout << sum;
+}
