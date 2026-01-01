51eefdcac78699af54c7a82eb24f6c5481bbbf3a
Sang <106306044+zzzz955@users.noreply.github.com>
2026-01-01 18:12:49 +0900

    [Gold V] Title: 행성 탐사, Time: 88 ms, Memory: 25504 KB -BaekjoonHub

---
+++ C++17/백준/Gold/5549. 행성 탐사/행성 탐사.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+const int N = 1001;
+int n, m, k;
+int js[N][N], os[N][N], is[N][N];
+int pj[N][N], po[N][N], pi[N][N];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			char c; cin >> c;
+			if (c == 'J') ++js[i][j];
+			else if (c == 'O') ++os[i][j];
+			else ++is[i][j];
+		}
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			pj[i][j] = pj[i][j - 1] + pj[i - 1][j] - pj[i - 1][j - 1] + js[i][j];
+			po[i][j] = po[i][j - 1] + po[i - 1][j] - po[i - 1][j - 1] + os[i][j];
+			pi[i][j] = pi[i][j - 1] + pi[i - 1][j] - pi[i - 1][j - 1] + is[i][j];
+		}
+	}
+
+	while (k--) {
+		int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+		cout << pj[x2][y2] - pj[x2][y1 - 1] - pj[x1 - 1][y2] + pj[x1 - 1][y1 - 1] << " "
+			<< po[x2][y2] - po[x2][y1 - 1] - po[x1 - 1][y2] + po[x1 - 1][y1 - 1] << " "
+			<< pi[x2][y2] - pi[x2][y1 - 1] - pi[x1 - 1][y2] + pi[x1 - 1][y1 - 1] << "\n";
+	}
+}
