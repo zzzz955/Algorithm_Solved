5e239449f63296ec7d3ddf8cefb4601aad99df55
Sang <106306044+zzzz955@users.noreply.github.com>
2026-01-08 16:55:23 +0900

    [Silver I] Title: 중앙값 제거, Time: 460 ms, Memory: 16108 KB -BaekjoonHub

--- C++17/백준/Silver/23758. 중앙값 제거/중앙값 제거.cc
+++ C++17/백준/Silver/23758. 중앙값 제거/중앙값 제거.cc
@@ -7,7 +7,6 @@ const int N = 2e6;
 int arr[N];
 int n;
 priority_queue<int> a;
-priority_queue<int, vector<int>, greater<int>> b;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -17,22 +16,17 @@ int main() {
 	for (int i = 0; i < n; ++i) cin >> arr[i];
 	sort(arr, arr + n);
 	for (int i = 0; i < n; ++i) {
-		if (i < (n + 1) / 2) a.push(arr[i]);
-		else b.push(arr[i]);
+		if (i < (n + 1) >> 1) a.push(arr[i]);
 	}
 
 	int ans = 0;
-	while (1) {
+	while (!a.empty()) {
 		int cur = a.top(); a.pop();
 		if (cur == 1) break;
 		
-		int next = cur;
-		for (; next > 1; next /= 2) ++ans;
-
-		a.push(next);
-		if (a.size() > b.size() + 1) {
-			b.push(a.top());
-			a.pop();
+		while (cur > 1) {
+			++ans;
+			cur >>= 1;
 		}
 	}
 	cout << ++ans;
