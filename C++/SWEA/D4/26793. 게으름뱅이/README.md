fbc24dcec8a32068c5682d3315459b016137cac8
전상혁 <106306044+zzzz955@users.noreply.github.com>
2026-05-13 16:57:49 +0900

    [D4] Title: 게으름뱅이, Time: 1,378 ms, Memory: 13,644 KB -BaekjoonHub

---
+++ C++/SWEA/D4/26793. 게으름뱅이/README.md
@@ -0,0 +1,15 @@
+# [D4] 게으름뱅이 - 26793 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZ3XsxFqSCvHBIPV) 
+
+### 성능 요약
+
+메모리: 13,644 KB, 시간: 1,378 ms, 코드길이: 642 Bytes
+
+### 제출 일자
+
+2026-05-13 16:57
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ C++/SWEA/D4/26793. 게으름뱅이/게으름뱅이.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace	std;
+using pii = pair< int, int >;
+
+const int N = 1e6;
+pii works[ N ];
+
+int main()
+{
+	int t; cin >> t;
+	while ( t-- )
+	{
+		int n; cin >> n;
+		for ( int i = 0; i < n; ++i )
+		{
+			int d, t; cin >> d >> t;
+			works[ i ] = { t, d };
+		}
+
+		sort( works, works + n, greater< pii >() );
+
+		int last_time = 1e9;
+		for ( int i = 0; i < n; ++i )
+		{
+			const pii& work = works[ i ];
+			int t = work.first;
+			int d = work.second;
+
+			if ( last_time > t )
+				last_time = t;
+
+			//cout << last_time << " ";
+			last_time -= d;
+		}
+		//cout << last_time << '\n';
+
+		cout << last_time << '\n';
+	}
+}
